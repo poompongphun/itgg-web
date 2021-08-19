65e2bc4240c4bf96a4575a4a84e4b49a1b9438f9
Poom <poompongphun@gmail.com>
2021-08-19 17:30:22 +0700

    add router.base

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,9 @@ export default {
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
-
+  router: {
+    base: '/itgg-web/'
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - itgg',
